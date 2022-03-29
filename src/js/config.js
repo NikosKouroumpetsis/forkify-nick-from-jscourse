f876491b7569a6078c4551a36fcbc25aff1294ba
NikosKouroumpetsis <kourou67s@gmail.com>
2022-03-29 11:29:00 +0300

    config changes

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'f338c20d-93fa-4e9f-b90b-305907d511ea';
 export const MODAL_CLOSE_SEC = 2.5;
